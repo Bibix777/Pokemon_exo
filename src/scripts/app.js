67d5689ad5beda6cfb72359000f5c9619239ddb9
briac dannequin <briac.dannequin@student.heaj.be>
2026-02-17 12:37:31 +0100

    en cours

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,30 @@
 "use strict";
+const form = document.querySelector('.formulaire');
+const div = document.querySelector('div');
+const ul = document.querySelector('.list');
 
-const url = 'https://web.mayfly.ovh/proxy/pokeapi.php?endpoint=pokemon/ditto';
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event){
+    event.preventDefault();
+    div.innerText = "";
+
+    const term = form.elements.search.value;
+    const url = "https://web.mayfly.ovh/proxy/pokeapi.php?endpoint=pokemon/"+ term;
+    fetch(url)
+        .then(function(res){
+            return res.json();
+        }) 
+        .then(function(data){ 
+            const img = document.createElement('img');
+            div.innerText = data.name;
+            img.src = data.sprites.front_default;
+            div.appendChild(img);
+            
+            const li = document.createElement('li');
+            li.innerText = data.stats
+            ul.appendChild(li);
+        })
+    
+};
