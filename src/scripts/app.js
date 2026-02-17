1c752259e7b47911f255d63d602385633594c626
briac dannequin <briac.dannequin@student.heaj.be>
2026-02-17 15:38:31 +0100

    JS fini

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,12 +9,14 @@ form.addEventListener('submit', onSubmit);
 function onSubmit(event){
     event.preventDefault();
     div.innerText = "";
+    ul.innerText = "";
 
     const term = form.elements.search.value;
     const url = "https://web.mayfly.ovh/proxy/pokeapi.php?endpoint=pokemon/"+ term;
     fetch(url)
         .then(function(res){
             return res.json();
+           
         }) 
         .then(function(data){ 
             const img = document.createElement('img');
@@ -22,9 +24,26 @@ function onSubmit(event){
             img.src = data.sprites.front_default;
             div.appendChild(img);
             
-            const li = document.createElement('li');
-            li.innerText = data.stats
-            ul.appendChild(li);
+            //boucle pour afficher les stats
+            data.abilities.forEach(function(item){
+                const li = document.createElement('li');
+                li.innerText = item.ability.name;
+                ul.appendChild(li);
+                
+            });
+
+            data.stats.forEach(function(item){
+                const li = document.createElement('li');
+                li.innerText = item.stat.name + " : " + item.base_stat;
+                ul.appendChild(li);
+            });
+
+            data.types.forEach(function(item){
+                const li = document.createElement('li');
+                li.innerText = item.type.name;
+                ul.appendChild(li);
+            });
+            
         })
     
 };
